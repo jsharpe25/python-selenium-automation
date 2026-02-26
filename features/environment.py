325f0e1d6aa90aeaa3d1a44a30eca1897dfda0c7
Joel S <joelandsum@gmail.com>
2026-02-26 16:17:23 -0600

    Update PSA file

--- features/environment.py
+++ features/environment.py
@@ -1,8 +1,12 @@
+import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.remote.client_config import ClientConfig
+from selenium.webdriver.remote.remote_connection import RemoteConnection
 from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
 from support.logger import logger
-from selenium.webdriver.chrome.options import Options
+
 
 ### RUN ALLURE REPORT ###
 # Install allure behave (first time only), use command line: pip install allure-behave
@@ -20,21 +24,41 @@ def browser_init(context, scenario_name):
     # context.driver = webdriver.Firefox()
     # context.driver = webdriver.Safari()
 
-    ### BROWSERSTACK ###
+    ### BROWSERSTACK WEB ###
     # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
     # bs_user = ''  # Add your user key
     # bs_key = ''  # Add your pass key
     # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
     # options = Options()
-    # bstack_options = {
-    #     'os': 'Windows',  # 'OS X' for macOS, 'Windows' for Windows OS
-    #     'osVersion': '11',  # Specify the version you want to use
+    # bstack_options = { # Define your options
+    #     'os': 'OS X',  # 'OS X' for macOS, 'Windows' for Windows OS
+    #     'osVersion': 'Tahoe',  # Specify the version you want to use
+    #     'browserName': 'Safari', # Specify the browser you want to use
     #     'browserVersion': 'latest', # 'latest' uses newest stable version
-    #     'browserName': 'Chrome',
     #     'sessionName': scenario_name # Automatically pulls name from Behave scenario
     # }
     # options.set_capability('bstack:options', bstack_options)
     # context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    ### BROWSERSTACK MOBILE ###
+    # bs_user = os.getenv("BROWSERSTACK_USERNAME")
+    # bs_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
+    # if not bs_user or not bs_key:
+    #     raise Exception("BrowserStack credentials not set in .env file.")
+    # client_config = ClientConfig(remote_server_addr="https://hub-cloud.browserstack.com/wd/hub", username=bs_user, password=bs_key)
+    # remote_connection = RemoteConnection(client_config=client_config)
+    # bstack_options = {
+    #     "osVersion": "16.0",
+    #     'deviceName' : 'Samsung Galaxy S24',
+    #     'browserName': 'Chrome',
+    #     "projectName": "Project Title",
+    #     "buildName": "software_build",
+    #     'sessionName': scenario_name,
+    # }
+    # options = Options()
+    # options.set_capability('bstack:options', bstack_options)
+    # context.driver = webdriver.Remote(command_executor=remote_connection, options=options)
+    # context.is_mobile = True
 
     ### MOBILE EMULATION ###
     # mobile_emulation = {"deviceName": "iPhone XR"}
